9261d9f80b0b162f1e2e470822cd89c106b495b9
becky <becky@micropelt.com>
2021-07-01 09:33:03 +0200

    Compute the CRC

--- src/crc.rs
+++ src/crc.rs
@@ -1,8 +1,26 @@
 
 use crate::common::MESSAGE_LENGTH;
 
-fn crc_fill(message: &mut [u8; MESSAGE_LENGTH]) {
+const CRC_LENGTH: usize = 2;
 
+pub fn fill(message: &mut [u8; MESSAGE_LENGTH]) {
+    let mut crc = 0xffff;
+
+    for i in 0..(message.len() - CRC_LENGTH) {
+        crc ^= message[i] as u16;
+
+        for _ in 0..8 {
+            if (crc & 0x0001) != 0 {
+                crc >>= 1;
+                crc ^= 0xa001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+
+    message[6] = (crc & 0x00ff) as u8;
+    message[7] = (crc >> 8) as u8;
 }
 
 #[cfg(test)]
