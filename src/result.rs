8c753cadd1df33884545d3914d9ec1a1aa2e63d4
Chris Couzens <ccouzens@gmail.com>
2022-01-12 20:47:17 +0000

    Use serialPort's error from implementation
    Use what's provided

--- src/result.rs
+++ src/result.rs
@@ -20,10 +20,7 @@ impl From<Hm305pError> for io::Error {
     fn from(err: Hm305pError) -> io::Error {
         match err {
             Hm305pError::Io(e) => e,
-            Hm305pError::SerialPort(serialport::Error {
-                kind: serialport::ErrorKind::Io(inner),
-                ..
-            }) => io::Error::new(inner, err),
+            Hm305pError::SerialPort(inner) => io::Error::from(inner),
             _ => io::Error::new(io::ErrorKind::Other, err),
         }
     }
