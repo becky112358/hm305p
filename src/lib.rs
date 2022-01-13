029f2f104293f512ea7cd79dab23ad9ac3911332
becky <becky@micropelt.com>
2022-01-13 12:29:46 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,31 @@ mod result;
 pub use result::Hm305pError;
 mod voltage;
 
+/// Get the instantaneous current consumption, in mA
 pub fn get_current_ma() -> Result<u16, Hm305pError> {
     message::send_and_receive(Request::Read(Action::CurrentmA))
 }
 
+/// Get the instantaneous voltage, in mV
+/// # Accuracy
+/// 10mV
 pub fn get_voltage_mv() -> Result<u16, Hm305pError> {
     message::send_and_receive(Request::Read(Action::VoltagemV))
 }
 
+/// Set the current limit, in mA
 pub fn set_current(current_ma: u16) -> Result<(), Hm305pError> {
     let _ = message::send_and_receive(Request::Write((Action::CurrentmA, current_ma)))?;
 
     Ok(())
 }
 
+/// Set the voltage, in mV
+/// # Accuracy
+/// 10mV, rounded down to the nearest 10mV
+/// # Note
+/// The driver reports success before completing the task, and can take a few seconds to
+/// complete the task.
 pub fn set_voltage(voltage_mv: u16) -> Result<(), Hm305pError> {
     let _ = message::send_and_receive(Request::Write((Action::VoltagemV, voltage_mv)))?;
 
